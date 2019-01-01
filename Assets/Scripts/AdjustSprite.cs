412a6436f6cc5c0a7443f7c569479736d9507586
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-01-01 14:12:48 +0800

    Initial commit

---
+++ Assets/Scripts/AdjustSprite.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustSprite : MonoBehaviour
+{
+    public enum AdjustEnum
+    {
+        None,
+        X,
+        Y,
+        XY,
+    }
+
+
+    private static float screenScale, aspectScale;
+    private static Camera mainCam;
+
+    public AdjustEnum adjustEnum;
+
+    private void Awake()
+    {
+        if (mainCam == null)
+        {
+            mainCam = Camera.main;
+            screenScale = (float)Screen.height / Screen.width;
+            aspectScale = mainCam.orthographicSize * 2 / Screen.height * Screen.width;
+        }
+        switch (adjustEnum)
+        {
+            case AdjustEnum.None:
+                break;
+            case AdjustEnum.X:
+                ResizeX();
+                break;
+            case AdjustEnum.Y:
+                ResizeY();
+                break;
+            case AdjustEnum.XY:
+                ResizeXY();
+                break;
+            default:
+                break;
+        }
+        Destroy(this);
+    }
+
+    public void ResizeX()
+    {
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr)
+        {
+            var sp = sr.sprite;
+            var scale = transform.localScale;
+
+            scale.x = aspectScale / sr.bounds.size.x;
+            transform.localScale = scale;
+        }
+    }
+
+    public void ResizeY()
+    {
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr)
+        {
+            var sp = sr.sprite;
+            var scale = transform.localScale;
+            scale.y = aspectScale / sr.bounds.size.y * Screen.height / Screen.width;
+            transform.localScale = scale;
+        }
+    }
+
+    public void ResizeXY()
+    {
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr)
+        {
+            var sp = sr.sprite;
+            var scale = transform.localScale;
+            scale.x = aspectScale / sr.bounds.size.x;
+            scale.y = aspectScale / sr.bounds.size.y * screenScale;
+            transform.localScale = scale;
+        }
+    }
+}
